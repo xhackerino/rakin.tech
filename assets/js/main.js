44d38883d718b1d0aff5264ba7f95cba6a804562
Ilia Rakin <60438838+xhackerino@users.noreply.github.com>
2024-03-25 03:10:07 +0200

    full rollback

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,188 +1,144 @@
 "use strict";
 
+const showPopup = (() => {
+    let timeoutId = null;
+    const popup = document.querySelector(".popup");
+    const popupText = document.querySelector(".popup-text");
+
+    const animation = [
+        { opacity: 1, offset: 0 },
+        { opacity: 0, offset: 0.4 },
+        { opacity: 0, offset: 0.6 },
+        { opacity: 1, offset: 1 },  
+    ];
+
+    const sleep = async (ms) => new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+
+    return async (content, isError = false, durationMs = 3000) => {
+        if (timeoutId !== null) {
+            clearTimeout(timeoutId);
+            popupText.animate(animation, 300);
+            await sleep(150);
+        }
+
+        popupText.textContent = content;
+
+        if (isError) {
+            popup.classList.add("error");
+        } else {
+            popup.classList.remove("error");
+        }
+
+        await sleep(10);
+
+        popup.classList.add("show");
+
+        timeoutId = setTimeout(() => popup.classList.remove("show"), durationMs);
+    }
+})();
+
+const now = () => new Date();
+
+const range = (start, end, base, value) => {
+    const keys = Array.from(
+        { length: end - start + 1 },
+        (_, i) => (start + i) % base
+    );
+
+    return keys.reduce(
+        (acc, cur) => ({ ...acc, [cur]: value }),
+        {}
+    );
+}
+
 const SLEEPING = "💤 Sleeping...";
 const BUSY = "⚠️ Busy. ⛔ Do Not Disturb";
 const AVAILABLE = "✅ Available for messaging, calls will be declined 📵";
 const WEEKEND = "✨ On a weekend. 📳 Enjoying real life 🏞️";
-const HOLIDAY = "🎉 It's a holiday! 🛫 Unavailable today, enjoying a great time with my family 👨‍👩‍👧‍👦️";
-const translations = {
-    en: {
-        pageTitle: "rakin.tech | Ilia Rakin",
-        greeting: "👋 Hello!",
-        homepageText: "This is the homepage of Ilia Rakin.",
-        currentStatusText: "Right now I’m: ⏳ Checking the schedule...",
-        contactMeTitle: "💬 Contact Me",
-        gmailContact: "📩 Gmail:",
-        corporateContact: "🎓 Corporate:",
-        telegramContact: "📩 Telegram (Business related issues ONLY):",
-        seeAlsoTitle: "👀 See Also",
-        footerText: "Rakin Ilia",
-        inspirationText: "Inspired by 🇵🇱 Teris Labendszki & 🇺🇦 Stepan Tikunov",
-    },
-    fr: {
-        pageTitle: "rakin.tech | Ilia Rakin",
-        greeting: "👋 Bonjour!",
-        homepageText: "Voici la page d'accueil d'Ilia Rakin.",
-        currentStatusText: "En ce moment, je suis : ⏳ En train de vérifier l'agenda...",
-        contactMeTitle: "💬 Contactez-moi",
-        gmailContact: "📩 Gmail:,
-        corporateContact: "🎓 Corporate:",
-        telegramContact: "📩 Telegram (Uniquement pour les questions professionnelles):",
-        seeAlsoTitle: "👀 Voir également",
-        footerText: "Rakin Ilia",
-        inspirationText: "Inspiré par 🇵🇱 Teris Labendszki & 🇺🇦 Stepan Tikunov",
-    },
-    es: {
-        pageTitle: "rakin.tech | Ilia Rakin",
-        greeting: "👋 ¡Hola!",
-        homepageText: "Esta es la página de inicio de Ilia Rakin.",
-        currentStatusText: "Ahora mismo estoy: ⏳ Revisando el calendario...",
-        contactMeTitle: "💬 Contáctame",
-        gmailContact: "📩 Gmail:",
-        corporateContact: "🎓 Corporativo:",
-        telegramContact: "📩 Telegram (Solo para asuntos de negocios):",
-        seeAlsoTitle: "👀 Ver también",
-        footerText: "Rakin Ilia",
-        inspirationText: "Inspirado por 🇵🇱 Teris Labendszki & 🇺🇦 Stepan Tikunov",
-    },
-    ar: {
-        pageTitle: "rakin.tech | إيليا راكين",
-        greeting: "👋 مرحبا!",
-        homepageText: "هذه هي الصفحة الرئيسية لإيليا راكين.",
-        currentStatusText: "في الوقت الحالي أنا: ⏳ أتحقق من الجدول الزمني...",
-        contactMeTitle: "💬 اتصل بي",
-        gmailContact: "📩 Gmail:",
-        corporateContact: "🎓 الشركات:",
-        telegramContact: "📩 تلغرام (فقط للمسائل المتعلقة بالأعمال):",
-        seeAlsoTitle: "👀 انظر أيضا",
-        footerText: "راكين إيليا",
-        inspirationText: "مستوحى من 🇵🇱 تيريس لابيندزكي & 🇺🇦 ستيبان تيكونوف",
-    },
-    de: {
-        pageTitle: "rakin.tech | Ilia Rakin",
-        greeting: "👋 Hallo!",
-        homepageText: "Dies ist die Startseite von Ilia Rakin.",
-        currentStatusText: "Im Moment bin ich: ⏳ Beim Überprüfen meines Zeitplans...",
-        contactMeTitle: "💬 Kontaktiere mich",
-        gmailContact: "📩 Gmail:",
-        corporateContact: "🎓 Firmen:",
-        telegramContact: "📩 Telegram (Nur für geschäftliche Angelegenheiten): @xhackerino",
-        seeAlsoTitle: "👀 Siehe auch",
-        footerText: "Rakin Ilia",
-        inspirationText: "Inspiriert von 🇵🇱 Teris Labendszki & 🇺🇦 Stepan Tikunov",
-    },
-    ru: {
-        pageTitle: "rakin.tech | Илья Ракин",
-        greeting: "👋 Привет!",
-        homepageText: "Это визитная карточка Ильи Ракина.",
-        currentStatusText: "Сейчас я: ⏳ Проверяю расписание...",
-        contactMeTitle: "💬 Связаться со мной",
-        gmailContact: "📩 Почта Гугл:",
-        corporateContact: "🎓 Корпоративная:",
-        telegramContact: "📩 Телеграмм (Только для деловых вопросов):",
-        seeAlsoTitle: "👀 Смотрите также",
-        footerText: "Ракин Илья",
-        inspirationText: "Вдохновлялся 🇵🇱 Терисом Лабендзки и 🇺🇦 Степаном Тикуновым",
+const HOLYDAY = "🎉 It's a holyday! 🛫 Unavaliable today, enjoying a great time with my family 👨‍👩‍👧‍👦️";
+
+const schedule = {
+    ...range(1, 5, 7, {
+        ...range(0, 6, 24, {
+            message: SLEEPING,
+            dnd: true,
+        }),
+        ...range(7, 17, 24, {
+            message: BUSY,
+            dnd: false,
+        }),
+        ...range(18, 23, 24, {
+            message: AVAILABLE,
+            dnd: false,
+        }),
+        ...range()
+    }),
+    ...range(6, 7, 7, {
+        ...range(0, 23, 24, {
+            message: WEEKEND,
+            dnd: false
+        }),
+    }),
+};
+
+const statusContainer = document.querySelector("#status");
+
+setInterval(() => {
+    const day = now().getUTCDay();
+    const hour = now().getUTCHours();
+
+    const status = schedule[day][hour];
+    if (status !== statusContainer.textContent) {
+        statusContainer.textContent = status.message;
     }
-    };
+}, 1000);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const languageSelect = document.getElementById('language-select');
-    const yearSpan = document.getElementById('year');
+const copyElements = document.querySelectorAll(".copy");
 
-    yearSpan.textContent = new Date().getFullYear();
-
-    languageSelect.addEventListener('change', function() {
-        const selectedLanguage = this.value;
-        translatePage(selectedLanguage);
-        localStorage.setItem('selectedLanguage', selectedLanguage);
-        adjustLayoutForRTL(selectedLanguage);
+for (const element of copyElements) {
+    element.addEventListener("click", () => {
+        navigator.clipboard.writeText(element.textContent.trim());
+        showPopup("Address copied to clipboard!");
     });
+}
 
-    const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
-    languageSelect.value = savedLanguage;
-    translatePage(savedLanguage);
-    adjustLayoutForRTL(savedLanguage);
+const contactLinks = document.querySelectorAll(".contact");
 
-    initCopyToClipboard();
-    initBannerToggle();
-    initStatusUpdate();
+for (const link of contactLinks) {
+    link.addEventListener("click", (event) => {
+        const day = now().getUTCDay();
+        const hour = now().getUTCHours();    
 
-    function translatePage(lang) {
-            for (const key in translations[lang]) {
-                const element = document.getElementById(key);
-                if (element) {
-                    element.textContent = translations[lang][key];
-                }
-            }
+        const status = schedule[day][hour];
+
+        if (status.dnd) {
+            event.preventDefault();
+            showPopup("Please, contact me later. I'm sleeping", true)
         }
+    })
+}
 
-        function adjustLayoutForRTL(lang) {
-            document.body.setAttribute('dir', lang === 'ar' ? 'rtl' : 'ltr');
-        }
+const yearSpan = document.querySelector("#year");
+yearSpan.textContent = now().getUTCFullYear();
 
-        function initCopyToClipboard() {
-            document.querySelectorAll('.copy').forEach(element => {
-                element.addEventListener('click', () => {
-                    navigator.clipboard.writeText(element.textContent.trim()).then(() => {
-                        showPopup("Address copied to clipboard!");
-                    }, () => {
-                        showPopup("Failed to copy address.", true);
-                    });
-                });
-            });
-        }
+const banner = document.querySelector(".banner");
 
-        function initBannerToggle() {
-            const banner = document.querySelector('.banner');
-            const bannerButton = document.querySelector('.show-banner');
-            bannerButton.addEventListener('click', () => banner.classList.toggle('show'));
-            banner.addEventListener('click', event => {
-                if (event.target === banner) {
-                    banner.classList.remove('show');
-                }
-            });
-        }
+const showBanner = () => {
+    banner.classList.add("show");
+}
 
-        function showPopup(content, isError = false, durationMs = 3000) {
-            const popup = document.querySelector(".popup");
-            const popupText = document.querySelector(".popup-text");
-            popupText.textContent = content;
-            popup.classList.toggle("error", isError);
-            popup.classList.add("show");
-            setTimeout(() => popup.classList.remove("show"), durationMs);
-        }
+const hideBanner = () => {
+    banner.classList.remove("show");
+}
 
-        function initStatusUpdate() {
-            updateStatus();
-            setInterval(updateStatus, 60000); // Update every minute
-        }
-
-    function updateStatus() {
-        const now = new Date();
-        const day = now.getDay(); // 0 (Sunday) to 6 (Saturday)
-        const hour = now.getHours(); // 0 to 23
-        const month = now.getMonth(); // 0 (January) to 11 (December)
-        const date = now.getDate(); // 1 to 31
-
-        // Example holiday: January 1st
-        const isHoliday = (month === 0 && date === 1);
-
-        let currentStatusMessage = AVAILABLE; // Default status
-
-        if (isHoliday) {
-            currentStatusMessage = HOLIDAY;
-        } else if (day === 0 || day === 6) {
-            currentStatusMessage = WEEKEND;
-        } else if (hour < 8 || hour >= 17) {
-            currentStatusMessage = SLEEPING;
-        } else {
-            currentStatusMessage = BUSY;
-        }
-
-        const statusContainer = document.getElementById("status");
-        if (statusContainer) {
-            statusContainer.textContent = currentStatusMessage;
-        }
+banner.addEventListener("click", (event) => {
+    if (event.target === banner) {
+        hideBanner();
     }
 });
+
+const bannerButton = document.querySelector(".show-banner");
+
+bannerButton.addEventListener("click", () => showBanner());
